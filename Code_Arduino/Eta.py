aefc2f04e0f3dc47752255e48832dc315656f102
Himdolion <yohan.duarte@laposte.net>
2019-03-08 12:04:47 +0100

    Update Eta.py

--- Code_Arduino/Eta.py
+++ Code_Arduino/Eta.py
@@ -64,3 +64,19 @@ new = MByte.dot(DByte.T)
 Data = np.vstack([Data.T,new]).T
 
 np.savetxt(Output, Data)
+
+Nlyse = (new^2)^(1/2)[900:]
+
+P = 950*9/15
+I = 0
+Val = 0
+Val2 = 0
+
+for i in range(P):
+	I += 1
+	val = Nlyse[i*15:(i+1)*15].max()
+	Val += val
+	Val2 += val*val
+
+print("Moyenne: "+str(Val/I)+"/n")
+print("Incertitude: "+str((sqrt((Val*Val)-Val2))/I)+"/n")
