43d047b65dda9048180f66bdb36c74f7aee8efd0
Pacidus <yohan.duarte@laposte.net>
2019-03-24 00:10:18 +0100

    dim. 24 mars 00:10

---
+++ Data/graphs/Radius.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np;
+import matplotlib;
+import matplotlib.pyplot as plt;
+from matplotlib import rc
+import pandas as pd
+import seaborn as sns
+
+rc('font',**{'family':'serif','serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+
+matplotlib.rcParams['text.usetex'] = True;
+matplotlib.rcParams['text.latex.unicode'] = True;
+
+load = np.loadtxt;
+
+plot = plt.plot;
+
+error = plt.errorbar;
+
+xlabel = plt.xlabel;
+
+ylabel = plt.ylabel;
+
+show = plt.show;
+
+set_xlabel = plt.xlabel;
+
+set_ylabel = plt.ylabel;
+
+grid = plt.grid;
+
+Data = load("./Radius.txt");
+
+X = Data[:];
+
+sns.distplot(X, norm_hist=True ,bins=40,kde=False);
+sns.kdeplot(X, bw=.05);
+plt.axvline(x=3.65175);
+plt.axvline(x=8.32);
+
+x = [3.65,4,5,6,7,8,8.32,9];
+
+plt.xticks(x,rotation=45)
+
+set_xlabel(r'Rayon en mm', fontsize=12)
+#set_ylabel(r'Accélération en ${\gamma_m/g}$', fontsize=12)
+
+#grid(True);
+
+plt.savefig('Radius.png', dpi=700);
+show();
