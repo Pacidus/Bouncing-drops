e93a2ec83a903129b21dcc0c1167a8c8a6cbc8ce
Pacidus <yohan.duarte@laposte.net>
2019-03-23 21:55:39 +0100

    sam. 23 mars 21:55

--- Data/graphs/Lambda.py
+++ Data/graphs/Lambda.py
@@ -4,6 +4,7 @@ import numpy as np;
 import matplotlib;
 import matplotlib.pyplot as plt;
 from matplotlib import rc
+from scipy.optimize import curve_fit
 
 rc('font',**{'family':'serif','serif':['Helvetica']})
 ## for Palatino and other serif fonts use:
@@ -37,12 +38,33 @@ X	= Data[:,1];
 Y	= Data[:,0];
 X_error	= Data[:,2];
 
+def func(x):
+	DPI = 2*np.pi;
+	g = 9.81;
+	Lambda = x*10**-3;
+	rho = 950;
+	sig = 20.7*10**-3;
+	return((2/DPI)*np.sqrt((DPI*g/Lambda)+((sig*(DPI**3)))/(rho*Lambda**3)));
+
+#popt, pcov = curve_fit(func, X, Y,bounds=(0, 1));
+
+#print(*popt,"+-",*pcov[0]);
+
+x = np.linspace(4,19.5,1000);
+y = func(x);
+
 set_ylabel(r'Fréquence en Hz', fontsize=12)
-set_xlabel(r'${F_\lambda}$ en mm', fontsize=12)
+
+set_xlabel(r'${\lambda_F}$ en mm', fontsize=12)
 
 error(X, Y, xerr=X_error, fmt='.', ecolor='red',color='blue', capsize=2.5);
 
 grid(True);
-plt.savefig('lambda_f.png', dpi=700);
+
+plt.savefig('lambda_fexp.png', dpi=700);
+
+plt.plot(x,y,"k--", label = r'$\frac{1}{\pi}\sqrt{}$');
+
+plt.savefig('lambda_fth.png', dpi=700);
 
 show();
