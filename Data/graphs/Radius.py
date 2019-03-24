c747e0167cc35e614fe53ee9af2ab3aafb3811cc
Pacidus <yohan.duarte@laposte.net>
2019-03-24 14:16:21 +0100

    dim. 24 mars 14:16

--- Data/graphs/Radius.py
+++ Data/graphs/Radius.py
@@ -38,18 +38,18 @@ Data = load("./Radius.txt");
 X = Data[:];
 
 sns.distplot(X, norm_hist=True ,bins=40,kde=False);
-sns.kdeplot(X, bw=.05);
-plt.axvline(x=3.65175);
-plt.axvline(x=8.32);
+sns.kdeplot(X, bw=.05, shade=True, color="orange");
+#plt.axvline(x=3.65175);
+#plt.axvline(x=8.32);
 
 x = [3.65,4,5,6,7,8,8.32,9];
 
 plt.xticks(x,rotation=45)
 
-set_xlabel(r'Rayon en mm', fontsize=12)
+set_xlabel(r'Diamètre en mm', fontsize=12)
 #set_ylabel(r'Accélération en ${\gamma_m/g}$', fontsize=12)
 
-#grid(True);
+grid(True);
 
 plt.savefig('Radius.png', dpi=700);
 show();
