882c4fbde3f983d30e53844a9e570887c5161d45
Pacidus <yohan.duarte@laposte.net>
2019-03-28 22:10:34 +0100

    jeu. 28 mars 22:10

--- Data/graphs/periode.py
+++ Data/graphs/periode.py
@@ -39,12 +39,13 @@ X_error	= Data[:,1];
 Y	= Data[:,2];
 Y_error	= Data[:,3];
 
-def func(x,a):
-	return(a*x);
+def func(x,a,b):
+	return(a*x+b);
 
 popt, pcov = curve_fit(func, X, Y, sigma = Y_error);
+perr = np.sqrt(np.diag(pcov));
 
-print(*popt,"+-",*pcov[0]);
+print(*popt,"+-",*perr)
 x = np.linspace(0,X.max()+1,1000);
 y = func(x,*popt);
 
@@ -58,7 +59,7 @@ grid(True);
 
 plt.savefig('period.png', dpi=700);
 
-plt.plot(x,y,"gray",label=r"$a = %.3f \pm %.3f $ \\ $f(x) = a \times x$" %(*popt,*pcov));
+plt.plot(x,y,"gray",label=r"~\\ $a = %.3f \pm %.3f $ \\ $b = %.3f \pm %.3f$ \\$f(x) = a \times x + b$" %(popt[0],perr[0],popt[1],perr[1]));
 error(X, Y, xerr=X_error, fmt='.', ecolor='red',color='blue', capsize=2.5);
 plt.legend();
 plt.savefig('period_fth.png', dpi=700);
